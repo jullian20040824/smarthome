27412b02798605d30751ff99a340378bf7b59357
unknown <jullianlin2003@gmail.com>
2020-10-05 16:48:26 +0800

    當警報入侵,傳Email給我

--- Arduinosmarthome/Arduinosmarthome.ino
+++ Arduinosmarthome/Arduinosmarthome.ino
@@ -1,12 +1,29 @@
 void setup() 
 {
   pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(2, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() 
 {
-  digitalWrite(12, HIGH);
-  delay(500);
+  if(digitalRead(2)==HIGH) {
+    Serial.println("alert");
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    tone(7, 1000, 500);
+    delay(500);
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+    tone(7, 800, 500);
+    delay(500);
+}
+else {
+  Serial.println("normal");
   digitalWrite(12, LOW);
-  delay(500);
+  digitalWrite(11, LOW);
+  delay(1000);
+ }
 }
